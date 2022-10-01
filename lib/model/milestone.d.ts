9151ed1ce84a44ca46e732b030a9e3d2b9a5bd09
Nicholas Stamas <nicholas.stamas@gmail.com>
2022-10-01 14:35:25 -0400

    export milestone

--- lib/model/milestone.d.ts
+++ lib/model/milestone.d.ts
@@ -10,10 +10,10 @@ export declare enum MilestoneKeys {
 }
 export interface Milestone {
     [MilestoneKeys.Id]?: string;
-    [MilestoneKeys.AchievedAt]: FirebaseFirestore.Timestamp;
-    [MilestoneKeys.Category]: string;
+    [MilestoneKeys.AchievedAt]?: FirebaseFirestore.Timestamp;
+    [MilestoneKeys.Category]?: string;
     [MilestoneKeys.Name]: string;
-    [MilestoneKeys.Description]: string;
-    [MilestoneKeys.ImageUrl]: string;
-    [MilestoneKeys.VideoUrl]: string;
+    [MilestoneKeys.Description]?: string;
+    [MilestoneKeys.ImageUrl]?: string;
+    [MilestoneKeys.VideoUrl]?: string;
 }
