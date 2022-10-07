79da716ac6f82f2ceec915ee6ae461466ac13c38
Nicholas Stamas <nicholas.stamas@gmail.com>
2022-10-07 10:09:18 -0400

    add milestone fields

--- lib/model/milestone.d.ts
+++ lib/model/milestone.d.ts
@@ -3,17 +3,25 @@ export declare enum MilestoneKeys {
     Id = "id",
     AchievedAt = "achievedAt",
     Category = "category",
+    CategoryOrder = "categoryOrder",
     Name = "name",
     Description = "description",
+    DescriptionAchieved = "descriptionAchieved",
     ImageUrl = "imageUrl",
-    VideoUrl = "videoUrl"
+    ImageAchievedUrl = "imageAchievedUrl",
+    VideoUrl = "videoUrl",
+    ShareVideoUrl = "shareVideoUrl"
 }
 export interface Milestone {
     [MilestoneKeys.Id]?: string;
     [MilestoneKeys.AchievedAt]?: FirebaseFirestore.Timestamp;
     [MilestoneKeys.Category]?: string;
+    [MilestoneKeys.CategoryOrder]?: number;
     [MilestoneKeys.Name]: string;
     [MilestoneKeys.Description]?: string;
+    [MilestoneKeys.DescriptionAchieved]?: string;
     [MilestoneKeys.ImageUrl]?: string;
+    [MilestoneKeys.ImageAchievedUrl]?: string;
     [MilestoneKeys.VideoUrl]?: string;
+    [MilestoneKeys.ShareVideoUrl]?: string;
 }
--- src/model/milestone.ts
+++ src/model/milestone.ts
@@ -4,18 +4,26 @@ export enum MilestoneKeys {
   Id = 'id',
   AchievedAt = 'achievedAt',
   Category = 'category',
+  CategoryOrder = 'categoryOrder',
   Name = 'name',
   Description = 'description',
+  DescriptionAchieved = 'descriptionAchieved',
   ImageUrl = 'imageUrl',
+  ImageAchievedUrl = 'imageAchievedUrl',
   VideoUrl = 'videoUrl',
+  ShareVideoUrl = 'shareVideoUrl',
 }
 
 export interface Milestone {
   [MilestoneKeys.Id]?: string;
   [MilestoneKeys.AchievedAt]?: FirebaseFirestore.Timestamp;
   [MilestoneKeys.Category]?: string;
+  [MilestoneKeys.CategoryOrder]?: number;
   [MilestoneKeys.Name]: string;
   [MilestoneKeys.Description]?: string;
+  [MilestoneKeys.DescriptionAchieved]?: string;
   [MilestoneKeys.ImageUrl]?: string;
+  [MilestoneKeys.ImageAchievedUrl]?: string;
   [MilestoneKeys.VideoUrl]?: string;
+  [MilestoneKeys.ShareVideoUrl]?: string;
 }
