da5106f7bef35c3da3080c46a9afcbf4dfb48afd
Nicholas Stamas <nicholas.stamas@gmail.com>
2023-04-29 14:11:47 -0400

    add core14 modal

--- lib/model/appState.d.ts
+++ lib/model/appState.d.ts
@@ -3,7 +3,6 @@ export declare enum AppKeys {
 }
 export interface TelegramState {
     seenOnboarding20221026?: boolean;
-    seenCardio14April2023?: boolean;
 }
 export interface AppState {
     [AppKeys.Telegram]: TelegramState;
